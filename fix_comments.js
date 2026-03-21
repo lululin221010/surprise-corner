0cd7b3edf42fae7286f07999c7f946f131fc09f2
lululin221010 <lululin221010@gmil.com>
2026-03-21 11:55:49 +0800

    fix: CommentSection 改用 novelId 統一留言區，修 useEffect 依賴

---
+++ fix_comments.js
@@ -0,0 +1,12 @@
+﻿const { MongoClient } = require('mongodb');
+const uri = require('fs').readFileSync('.env.local','utf8').match(/MONGODB_URI=(.+)/)[1].trim();
+MongoClient.connect(uri).then(client => {
+  const db = client.db('SurpriseCornerDB');
+  return db.collection('chapter_comments').updateMany(
+    { chapterId: { $regex: '^lulu-s-' } },
+    { $set: { chapterId: 'lulu-life' } }
+  ).then(r => {
+    console.log('更新筆數：', r.modifiedCount);
+    client.close();
+  });
+}).catch(console.error);
